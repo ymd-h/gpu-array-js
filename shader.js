1ef02cc6a26ef45f5ab50bcf04abe60e155b1bf9
Yamada <ymd_h@outlook.jp>
2024-02-12 18:48:44 +0900

    Add indirect vector_op

--- shader.js
+++ shader.js
@@ -18,6 +18,50 @@ fn main(@builtin(global_invocation_id) id: vec3<u32>){
 }
 `;
 
+const vector_op_indirect = (
+    op, size,
+    lhs, rhs, out,
+    lhs_strides, rhs_strides, out_strides,
+) => `
+@group(0) @binding(${lhs.binding})
+var<storage, read> lhs: array<${lhs.type}>;
+
+@group(0) @binding(${rhs.binding})
+var<storage, read> rhs: array<${rhs.type}>;
+
+@group(0) @binding(${out.binding})
+var<storage, read_write> out: array<${out.type}>;
+
+@group(0) @binding(${lhs_strides.binding})
+var<storage, read> lhs_strides: array<u32>;
+
+@group(0) @binding(${rhs_strides.binding})
+var<storage, read> rhs_strides: array<u32>;
+
+@group(0) @binding(${out_strides.binding})
+var<storage, read> out_strides: array<u32>;
+
+@compute @workgroup_size(${size})
+fn main(@builtin(global_invocation_id) id: vec3<u32>){
+    if(id.x >= arrayLength(&out)){ return; }
+
+    u32 O = id.x;
+    u32 L = 0;
+    u32 R = 0;
+    for(u32 s = arrayLength(&out_stride) -1; s > 0; s--){
+        u32 i = O % out_strides[s-1];
+        L += i * lhs_strides[s];
+        R += i * rhs_strides[s];
+        O -= i;
+    }
+    L += O * lhs_strides[0];
+    R += O * rhs_strides[0];
+
+    out[id.x] = ${out.conv}(${lhs.conv}(lhs[L]) ${op} ${rhs.conv}(rhs[R]));
+}
+`;
+
+
 const func1 = (f, size, arg, out) => `
 @group(0) @binding(${arg.binding})
 var<storage, read> arg: array<${arg.type}>;
@@ -53,4 +97,7 @@ fn main(@builtin(global_invocation_id) id: vec3<u32>){
 `;
 
 
-export { vector_op, func1, func2 };
+export {
+    vector_op, vector_op_indirect,
+    func1, func2,
+};
