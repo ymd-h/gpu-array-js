b950bd57db7807a5fa0482a05dce39d812658b34
Yamada <ymd_h@outlook.jp>
2024-02-12 22:38:13 +0900

    Implement func2_indirect shader

--- shader.js
+++ shader.js
@@ -96,8 +96,49 @@ fn main(@builtin(global_invocation_id) id: vec3<u32>){
 }
 `;
 
+const func2_indirect = (f, size, args, out, args_strides, out_strides) => `
+@group(0) @binding(${args[0].binding})
+var<storage, read> arg0: array<${args[0].type}>;
+
+@group(0) @binding(${args[1].binding})
+var<storage, read> arg1: array<${args[1].type}>;
+
+@group(0) @binding(${out.binding})
+var<storage, read_write> out: array<${out.type}>;
+
+@group(0) @binding(${args_strides[0].binding})
+var<storage, read> arg0_strides: array<u32>;
+
+@group(0) @binding(${args_strides[1].binding})
+var<storage, read> arg1_strides: array<u32>;
+
+@group(0) @binding(${out_strides.binding})
+var<storage, read> out_strides: array<u32>;
+
+@compute @workgroup_size(${size})
+fn main(@builtin(global_invocation_id) id: vec3<u32>){
+    if(id.x >= arrayLength(&out)){ return; }
+
+    u32 O = id.x;
+    u32 I0 = 0;
+    u32 I1 = 0;
+    for(u32 s = arrayLength(&out_strides) -1; s > 0; s--){
+        u32 i = O % out_strides[s-1];
+        I0 += i * arg0_strides[s];
+        I1 += i * arg1_strides[s];
+        O -= i;
+    }
+    I0 += O * arg0_strides[0];
+    I1 += O * arg1_strides[0];
+
+    out[id.x] = ${out.conv}(${f}(${args[0].conv}(arg0[I0]),
+                                 ${args[1].conv}(arg1[I1])));
+}
+`;
+
 
 export {
     vector_op, vector_op_indirect,
-    func1, func2,
+    func1,
+    func2, func2_indirect,
 };
